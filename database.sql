e3c2a53b88829a162f62ba773db11974c310eecc
Gabriel Marques <amarques.gabriel@gmail.com>
2020-05-03 16:02:23 -0300

    Translating database and adding VSCode debug settings

--- database.sql
+++ database.sql
@@ -11,295 +11,299 @@ Database: PostgreSQL 9.4
 
 -- Create tables section -------------------------------------------------
 
--- Table usuario
+-- Table user
 
-CREATE TABLE "usuario"
+CREATE TABLE "user"
 (
   "id" Serial NOT NULL,
-  "nome" Character varying(50) NOT NULL,
-  "senha" Character varying NOT NULL,
+  "name" Character varying(50) NOT NULL,
+  "password" Character varying NOT NULL,
   "email" Character varying(30) NOT NULL,
-  "nascimento" Date NOT NULL
+  "birth_date" Date NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "usuario" ADD CONSTRAINT "Key5" PRIMARY KEY ("id")
+CREATE UNIQUE INDEX UNIQUE_email ON "user"("email")
 ;
--- Table exercicio
 
-CREATE TABLE "exercicio"
+ALTER TABLE "user" ADD CONSTRAINT "Key5" PRIMARY KEY ("id")
+;
+
+-- Table exercise
+
+CREATE TABLE "exercise"
 (
   "id" Serial NOT NULL,
-  "nome" Character varying(20) NOT NULL,
-  "descricao" Character varying(50) NOT NULL
+  "name" Character varying(20) NOT NULL,
+  "description" Character varying(50) NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "exercicio" ADD CONSTRAINT "Key6" PRIMARY KEY ("id")
+ALTER TABLE "exercise" ADD CONSTRAINT "Key6" PRIMARY KEY ("id")
 ;
--- Table ficha
+-- Table workout_plan
 
-CREATE TABLE "ficha"
+CREATE TABLE "workout_plan"
 (
   "id" Serial NOT NULL,
-  "id_usuario" Integer NOT NULL,
-  "data" Date NOT NULL,
-  "data_troca" Date NOT NULL,
-  "descricao" Character varying(255)
+  "user_id" Integer NOT NULL,
+  "date" Date NOT NULL,
+  "change_date" Date NOT NULL,
+  "description" Character varying(255)
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "ficha" ADD CONSTRAINT "Key7" PRIMARY KEY ("id","id_usuario")
+ALTER TABLE "workout_plan" ADD CONSTRAINT "Key7" PRIMARY KEY ("id","user_id")
 ;
--- Table avaliacao
+-- Table assessment
 
-CREATE TABLE "avaliacao"
+CREATE TABLE "assessment"
 (
   "id" Serial NOT NULL,
-  "id_usuario" Integer NOT NULL,
-  "data" Date NOT NULL,
-  "peso" Numeric(5,2) NOT NULL,
-  "percentual_gordura" Numeric(5,2) NOT NULL,
-  "pescoco" Numeric(6,2) NOT NULL,
-  "ombro" Numeric(6,2) NOT NULL,
+  "user_id" Integer NOT NULL,
+  "date" Date NOT NULL,
+  "weight" Numeric(5,2) NOT NULL,
+  "fat_percentage" Numeric(5,2) NOT NULL,
+  "neck" Numeric(6,2) NOT NULL,
+  "shoulder" Numeric(6,2) NOT NULL,
   "torax" Numeric(6,2) NOT NULL,
   "abdomen" Numeric(6,2) NOT NULL,
-  "cintura" Numeric(6,2) NOT NULL,
-  "quadril" Numeric(6,2) NOT NULL,
-  "braco_esquerdo" Numeric(6,2) NOT NULL,
-  "braco_direito" Numeric(6,2) NOT NULL,
-  "antebraco_esquerdo" Numeric(6,2) NOT NULL,
-  "antebraco_direito" Numeric(6,2) NOT NULL,
-  "coxa_esquerda" Numeric(6,2) NOT NULL,
-  "coxa_direita" Numeric(6,2) NOT NULL,
-  "panturrilha_esquerda" Numeric(6,2) NOT NULL,
-  "panturrilha_direita" Numeric(6,2) NOT NULL
+  "waist" Numeric(6,2) NOT NULL,
+  "hip" Numeric(6,2) NOT NULL,
+  "left_arm" Numeric(6,2) NOT NULL,
+  "right_arm" Numeric(6,2) NOT NULL,
+  "left_forearm" Numeric(6,2) NOT NULL,
+  "right_forearm" Numeric(6,2) NOT NULL,
+  "left_thigh" Numeric(6,2) NOT NULL,
+  "right_thigh" Numeric(6,2) NOT NULL,
+  "left_calf" Numeric(6,2) NOT NULL,
+  "right_calf" Numeric(6,2) NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "avaliacao" ADD CONSTRAINT "Key8" PRIMARY KEY ("id","id_usuario")
+ALTER TABLE "assessment" ADD CONSTRAINT "Key8" PRIMARY KEY ("id","user_id")
 ;
--- Table treino
+-- Table workout
 
-CREATE TABLE "treino"
+CREATE TABLE "workout"
 (
   "id" Serial NOT NULL,
-  "id_ficha" Integer NOT NULL,
-  "id_usuario" Integer NOT NULL,
-  "numero_treino" Smallint NOT NULL,
-  "descricao" Character varying(255)
+  "workout_plan_id" Integer NOT NULL,
+  "user_id" Integer NOT NULL,
+  "workout_number" Smallint NOT NULL,
+  "description" Character varying(255)
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "treino" ADD CONSTRAINT "Key14" PRIMARY KEY ("id_ficha","id","id_usuario")
+ALTER TABLE "workout" ADD CONSTRAINT "Key14" PRIMARY KEY ("workout_plan_id","id","user_id")
 ;
--- Table musculo
+-- Table muscle
 
-CREATE TABLE "musculo"
+CREATE TABLE "muscle"
 (
   "id" Serial NOT NULL,
-  "nome" Character varying NOT NULL
+  "name" Character varying NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "musculo" ADD CONSTRAINT "Key16" PRIMARY KEY ("id")
+ALTER TABLE "muscle" ADD CONSTRAINT "Key16" PRIMARY KEY ("id")
 ;
--- Table musculo_exercicio
+-- Table exercice_muscle
 
-CREATE TABLE "musculo_exercicio"
+CREATE TABLE "exercice_muscle"
 (
-  "id_exercicio" Integer NOT NULL,
-  "id_musculo" Integer NOT NULL
+  "exercise_id" Integer NOT NULL,
+  "muscle_id" Integer NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "musculo_exercicio" ADD CONSTRAINT "Key18" PRIMARY KEY ("id_exercicio","id_musculo")
+ALTER TABLE "exercice_muscle" ADD CONSTRAINT "Key18" PRIMARY KEY ("exercise_id","muscle_id")
 ;
--- Table aparelho
+-- Table equipment
 
-CREATE TABLE "aparelho"
-(
-  "id" Serial NOT NULL,
-  "nome" Character varying NOT NULL
-)
-WITH (
-  autovacuum_enabled=true)
-;
-
-ALTER TABLE "aparelho" ADD CONSTRAINT "Key19" PRIMARY KEY ("id")
-;
--- Table aparelho_exercicio
-
-CREATE TABLE "aparelho_exercicio"
-(
-  "id_exercicio" Integer NOT NULL,
-  "id_aparelho" Integer NOT NULL
-)
-WITH (
-  autovacuum_enabled=true)
-;
-
-ALTER TABLE "aparelho_exercicio" ADD CONSTRAINT "Key20" PRIMARY KEY ("id_exercicio","id_aparelho")
-;
--- Table atividade
-
-CREATE TABLE "atividade"
+CREATE TABLE "equipment"
 (
   "id" Serial NOT NULL,
-  "id_ficha" Integer NOT NULL,
-  "id_treino" Integer NOT NULL,
-  "id_usuario" Integer NOT NULL,
-  "id_exercicio" Integer NOT NULL,
-  "id_aparelho" Integer NOT NULL,
-  "series" Smallint NOT NULL,
-  "ordem_execucao" Smallint NOT NULL,
-  "peso" Smallint,
-  "repeticoes" Character varying(20),
-  "observacoes" Character varying(255)
+  "name" Character varying NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "atividade" ADD CONSTRAINT "Key21" PRIMARY KEY ("id_exercicio","id_aparelho","id_ficha","id_treino","id_usuario","id")
+ALTER TABLE "equipment" ADD CONSTRAINT "Key19" PRIMARY KEY ("id")
 ;
--- Table objetivo
+-- Table exercise_equipment
 
-CREATE TABLE "objetivo"
+CREATE TABLE "exercise_equipment"
+(
+  "exercise_id" Integer NOT NULL,
+  "equipment_id" Integer NOT NULL
+)
+WITH (
+  autovacuum_enabled=true)
+;
+
+ALTER TABLE "exercise_equipment" ADD CONSTRAINT "Key20" PRIMARY KEY ("exercise_id","equipment_id")
+;
+-- Table activity
+
+CREATE TABLE "activity"
 (
   "id" Serial NOT NULL,
-  "nome" Character varying NOT NULL,
-  "descricao" Character varying NOT NULL
+  "workout_plan_id" Integer NOT NULL,
+  "workout_id" Integer NOT NULL,
+  "user_id" Integer NOT NULL,
+  "exercise_id" Integer NOT NULL,
+  "equipment_id" Integer NOT NULL,
+  "sets" Smallint NOT NULL,
+  "execution_order" Smallint NOT NULL,
+  "weight" Smallint,
+  "repetitions" Character varying(20),
+  "observations" Character varying(255)
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "objetivo" ADD CONSTRAINT "Key22" PRIMARY KEY ("id")
+ALTER TABLE "activity" ADD CONSTRAINT "Key21" PRIMARY KEY ("exercise_id","equipment_id","workout_plan_id","workout_id","user_id","id")
 ;
--- Table objetivo_ficha
+-- Table goal
 
-CREATE TABLE "objetivo_ficha"
-(
-  "id_objetivo" Integer NOT NULL,
-  "id_ficha" Integer NOT NULL,
-  "id_usuario" Integer NOT NULL
-)
-WITH (
-  autovacuum_enabled=true)
-;
-
-ALTER TABLE "objetivo_ficha" ADD CONSTRAINT "Key23" PRIMARY KEY ("id_objetivo","id_ficha","id_usuario")
-;
--- Table dia_atividade
-
-CREATE TABLE "dia_atividade"
+CREATE TABLE "goal"
 (
   "id" Serial NOT NULL,
-  "id_atividade" Integer NOT NULL,
-  "id_exercicio" Integer NOT NULL,
-  "nro_aparelho" Integer NOT NULL,
-  "id_ficha" Integer NOT NULL,
-  "id_treino" Integer NOT NULL,
-  "id_usuario" Integer NOT NULL,
-  "data_inicio" Date NOT NULL,
-  "data_fim" Date
+  "name" Character varying NOT NULL,
+  "description" Character varying NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "dia_atividade" ADD CONSTRAINT "Key24" PRIMARY KEY ("id_exercicio","nro_aparelho","id","id_ficha","id_treino","id_usuario","id_atividade")
+ALTER TABLE "goal" ADD CONSTRAINT "Key22" PRIMARY KEY ("id")
 ;
--- Table categoria_exercicio
+-- Table workout_plan_goal
 
-CREATE TABLE "categoria_exercicio"
+CREATE TABLE "workout_plan_goal"
+(
+  "id_goal" Integer NOT NULL,
+  "workout_plan_id" Integer NOT NULL,
+  "user_id" Integer NOT NULL
+)
+WITH (
+  autovacuum_enabled=true)
+;
+
+ALTER TABLE "workout_plan_goal" ADD CONSTRAINT "Key23" PRIMARY KEY ("id_goal","workout_plan_id","user_id")
+;
+-- Table activity_day
+
+CREATE TABLE "activity_day"
 (
   "id" Serial NOT NULL,
-  "nome" Character varying(50) NOT NULL,
-  "descricao" Character varying(255)
+  "activity_id" Integer NOT NULL,
+  "exercise_id" Integer NOT NULL,
+  "equipment_number" Integer NOT NULL,
+  "workout_plan_id" Integer NOT NULL,
+  "workout_id" Integer NOT NULL,
+  "user_id" Integer NOT NULL,
+  "start_date" Date NOT NULL,
+  "end_date" Date
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "categoria_exercicio" ADD CONSTRAINT "PK_categoria_exercicio" PRIMARY KEY ("id")
+ALTER TABLE "activity_day" ADD CONSTRAINT "Key24" PRIMARY KEY ("exercise_id","equipment_number","id","workout_plan_id","workout_id","user_id","activity_id")
 ;
--- Table modificadores_atividades
+-- Table exercise_category
 
-CREATE TABLE "modificadores_atividades"
+CREATE TABLE "exercise_category"
 (
-  "cod_exercicio" Integer NOT NULL,
-  "id_aparelho" Integer NOT NULL,
-  "id_modificador" Integer NOT NULL,
-  "id_ficha" Integer NOT NULL,
-  "id_treino" Integer NOT NULL,
-  "id_usuario" Integer NOT NULL,
-  "id_atividade" Integer NOT NULL,
-  "observacoes" Character varying(255)
+  "id" Serial NOT NULL,
+  "name" Character varying(50) NOT NULL,
+  "description" Character varying(255)
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE "modificadores_atividades" ADD CONSTRAINT "PK_modificadores_atividades" PRIMARY KEY ("cod_exercicio","id_aparelho","id_modificador","id_ficha","id_treino","id_usuario","id_atividade")
+ALTER TABLE "exercise_category" ADD CONSTRAINT "PK_exercise_category" PRIMARY KEY ("id")
+;
+-- Table activities_modifiers
+
+CREATE TABLE "activities_modifiers"
+(
+  "exercise_id" Integer NOT NULL,
+  "equipment_id" Integer NOT NULL,
+  "modifier_id" Integer NOT NULL,
+  "workout_plan_id" Integer NOT NULL,
+  "workout_id" Integer NOT NULL,
+  "user_id" Integer NOT NULL,
+  "activity_id" Integer NOT NULL,
+  "observations" Character varying(255)
+)
+WITH (
+  autovacuum_enabled=true)
+;
+
+ALTER TABLE "activities_modifiers" ADD CONSTRAINT "PK_activities_modifiers" PRIMARY KEY ("exercise_id","equipment_id","modifier_id","workout_plan_id","workout_id","user_id","activity_id")
 ;
 -- Create foreign keys (relationships) section ------------------------------------------------- 
 
-ALTER TABLE "treino" ADD CONSTRAINT "Relationship17" FOREIGN KEY ("id_ficha", "id_usuario") REFERENCES "ficha" ("id", "id_usuario") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "workout" ADD CONSTRAINT "Relationship17" FOREIGN KEY ("workout_plan_id", "user_id") REFERENCES "workout_plan" ("id", "user_id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "musculo_exercicio" ADD FOREIGN KEY ("id_exercicio") REFERENCES "exercicio" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "exercice_muscle" ADD FOREIGN KEY ("exercise_id") REFERENCES "exercise" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "musculo_exercicio" ADD FOREIGN KEY ("id_musculo") REFERENCES "musculo" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "exercice_muscle" ADD FOREIGN KEY ("muscle_id") REFERENCES "muscle" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "aparelho_exercicio" ADD FOREIGN KEY ("id_exercicio") REFERENCES "exercicio" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "exercise_equipment" ADD FOREIGN KEY ("exercise_id") REFERENCES "exercise" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "aparelho_exercicio" ADD CONSTRAINT "Relationship24" FOREIGN KEY ("id_aparelho") REFERENCES "aparelho" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "exercise_equipment" ADD CONSTRAINT "Relationship24" FOREIGN KEY ("equipment_id") REFERENCES "equipment" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "atividade" ADD FOREIGN KEY ("id_exercicio", "id_aparelho") REFERENCES "aparelho_exercicio" ("id_exercicio", "id_aparelho") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "activity" ADD FOREIGN KEY ("exercise_id", "equipment_id") REFERENCES "exercise_equipment" ("exercise_id", "equipment_id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "atividade" ADD FOREIGN KEY ("id_ficha", "id_treino", "id_usuario") REFERENCES "treino" ("id_ficha", "id", "id_usuario") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "activity" ADD FOREIGN KEY ("workout_plan_id", "workout_id", "user_id") REFERENCES "workout" ("workout_plan_id", "id", "user_id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "objetivo_ficha" ADD FOREIGN KEY ("id_objetivo") REFERENCES "objetivo" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "workout_plan_goal" ADD FOREIGN KEY ("id_goal") REFERENCES "goal" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "dia_atividade" ADD CONSTRAINT "Relationship29" FOREIGN KEY ("id_exercicio", "nro_aparelho", "id_ficha", "id_treino", "id_usuario", "id_atividade") REFERENCES "atividade" ("id_exercicio", "id_aparelho", "id_ficha", "id_treino", "id_usuario", "id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "activity_day" ADD CONSTRAINT "Relationship29" FOREIGN KEY ("exercise_id", "equipment_number", "workout_plan_id", "workout_id", "user_id", "activity_id") REFERENCES "activity" ("exercise_id", "equipment_id", "workout_plan_id", "workout_id", "user_id", "id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "modificadores_atividades" ADD FOREIGN KEY ("cod_exercicio", "id_aparelho", "id_ficha", "id_treino", "id_usuario", "id_atividade") REFERENCES "atividade" ("id_exercicio", "id_aparelho", "id_ficha", "id_treino", "id_usuario", "id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "activities_modifiers" ADD FOREIGN KEY ("exercise_id", "equipment_id", "workout_plan_id", "workout_id", "user_id", "activity_id") REFERENCES "activity" ("exercise_id", "equipment_id", "workout_plan_id", "workout_id", "user_id", "id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "modificadores_atividades" ADD FOREIGN KEY ("id_modificador") REFERENCES "categoria_exercicio" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "activities_modifiers" ADD FOREIGN KEY ("modifier_id") REFERENCES "exercise_category" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "ficha" ADD FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "workout_plan" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "objetivo_ficha" ADD FOREIGN KEY ("id_ficha", "id_usuario") REFERENCES "ficha" ("id", "id_usuario") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "workout_plan_goal" ADD FOREIGN KEY ("workout_plan_id", "user_id") REFERENCES "workout_plan" ("id", "user_id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
-ALTER TABLE "avaliacao" ADD FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE "assessment" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
 
