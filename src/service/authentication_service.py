1ea46dc6f67fc984f26f8ea3fbafb025fb56f6ea
Gabriel Marques <amarques.gabriel@gmail.com>
2020-05-03 21:44:39 -0300

    Moving authentication logic to AuthenticationService

--- src/service/authentication_service.py
+++ src/service/authentication_service.py
@@ -1,12 +1,13 @@
-from flask_jwt import JWT
-
 from repository.users_repository import UsersRepository
+from config.bcrypt import bcrypt
+class AuthenticationService:
+    @classmethod
+    def authenticate(cls, email, password):
+        user = UsersRepository.get_user_by_email(email)
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
 
-def authenticate(email, password):
-    user = UsersRepository.get_user_by_email(email)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-        return user
-
-def identity(payload):
-    user_id = payload['identity']
-    return UsersRepository.get_user_by_id(user_id)
+    @classmethod
+    def get_authenticated_user(cls, payload):
+        user_id = payload['identity']
+        return UsersRepository.get_user_by_id(user_id)
