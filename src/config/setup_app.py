1274cdad9d7f26fa4a117d0f323180dc73a0225e
Gabriel Marques <amarques.gabriel@gmail.com>
2020-05-11 22:20:18 -0300

    Fixing CORS issues on POST calls

--- src/config/setup_app.py
+++ src/config/setup_app.py
@@ -3,15 +3,25 @@ from flask_cors import CORS
 from controller.api_controller import register_controllers
 
 
-from .jwt import setup_jwt
-from .bcrypt import setup_bcrypt
 from .database import setup_database
+from .bcrypt import setup_bcrypt
+from .jwt import setup_jwt
 
 
 def setup_app(app):
-    app.config['SECRET_KEY'] = 'super-secret'
+    # Setup the connection to the database
     setup_database(app)
+    
+    # Setup BCrypt for password handling
     setup_bcrypt(app)
+    
+    # Register the API endpoints
     register_controllers(app)
+
+    # Setup JWT for user authentication
+    app.config['SECRET_KEY'] = 'super-secret'
     setup_jwt(app)
+
+    # Fixes CORS issues
+    app.config['CORS_HEADERS'] = 'Content-Type'
     CORS(app)
