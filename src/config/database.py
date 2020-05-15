f3452b62c2c7336a6feb4b8e3bc381c29ba0fec8
Gabriel Marques <amarques.gabriel@gmail.com>
2020-05-15 00:03:34 -0300

    Creating migrations for user and assessment

--- src/config/database.py
+++ src/config/database.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from .environment import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_URL, POSTGRES_DB
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def setup_database(app):
     db_url = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
@@ -11,3 +12,4 @@ def setup_database(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    migrate.init_app(app, db)
--- src/model/user.py
+++ src/model/user.py
@@ -4,7 +4,7 @@ from config.database import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
-    email = db.Column(db.String(30))
+    email = db.Column(db.String(30), unique=True)
     password = db.Column(db.String())
     birth_date = db.Column(db.DateTime)
 
